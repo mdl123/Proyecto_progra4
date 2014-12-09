7b9d7c3226110cd47f0208904e28538077ab5c93
jcfloresnaj <jcflores0288@unitec.edu>
2014-12-09 16:15:56 -0600

    Fecha actual cancelar citas

--- ArchivosPhp/cancelarcitas2.php
+++ ArchivosPhp/cancelarcitas2.php
@@ -5,7 +5,7 @@
    if($enlace){
      //echo "Conexion establecida satisfactoriamente, ";
      $Conn = 'SELECT * FROM citas where id_doctor = ';
-     $Conn.= mysqli_real_escape_string($enlace,$id).' and '." CitaCancelada = '0';";
+     $Conn.= mysqli_real_escape_string($enlace,$id).' and '." CitaCancelada = '0' and Fecha_cita >= CURDATE();";
      $query = @mysqli_query($enlace,$Conn);
      if($query){
          while ($row = mysqli_fetch_assoc($query)) {
